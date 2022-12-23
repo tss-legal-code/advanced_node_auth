3d2015f8ae1e1ec0e217ab6ec030f6adb00c2da3
tss-legal-code <tss.legal.code@gmail.com>
2022-12-23 23:10:25 +0200

    add user, token and mail service

--- server/service/mail-service.js
+++ server/service/mail-service.js
@@ -1,6 +1,36 @@
-class MailService {
-  async sendActivationMail(to, link) {
+const nodemailer = require('nodemailer');
 
+class MailService {
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      // service: 'gmail',
+      host: process.env.SMTP_HOST,
+      port: process.env.SMTP_PORT,
+      secure: false,
+      auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD
+      },
+      tls: {
+        // do not fail on invalid certs
+        rejectUnauthorized: false,
+      },
+    });
+  }
+
+  async sendActivationMail(to, link) {
+    await this.transporter.sendMail({
+      from: 'tss.nodemailer@gmail.com', //process.env.SMTP_USER,
+      to,
+      subject: 'Activation of your account at TSS ' + process.env.API_URL,
+      text: "",
+      html: `
+      <div>
+        <h1>To activate your account at TSS click an activation link below:</h1>
+        <a href="${link}">${link}</a>
+      </div>
+      `
+    });
   }
 }
 
--- server/service/user-service.js
+++ server/service/user-service.js
@@ -14,7 +14,7 @@ class UserService {
     const activationLink = uuid.v4();
     const passwordHash = await bcrypt.hash(password, 3);
     const user = await UserModel.create({ email, password: passwordHash, activationLink });
-    await mailService.sendActivationMail(email, activationLink);
+    await mailService.sendActivationMail(email, `${process.env.API_URL}/api/activate/${activationLink}`);
     const userDto = new UserDto(user); // extract just necessary payload
     const tokens = tokenService.generateTokens({ ...userDto });
     await tokenService.saveToken(userDto.id, tokens.refreshToken);
